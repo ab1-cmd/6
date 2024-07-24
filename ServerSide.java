efe8142cf91be2ff54ebcde399876ee1b2ef4cdb
ab1-cmd <cab031174@gmail.com>
2024-07-24 10:18:44 +0530

    Update ServerSide.java

--- ServerSide.java
+++ ServerSide.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package anilkali;
+
 import java.io.*;
 import java.net.*;
 
-/**
- *
- * @author anilk
- */
 public class ServerSide {
     public static void main(String[]args)
     {
