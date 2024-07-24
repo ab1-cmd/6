6139b35c7ff90d5ef62285f2d3a9b70c30709eed
ab1-cmd <cab031174@gmail.com>
2024-07-24 10:18:00 +0530

    Update ClientSide.java

--- ClientSide.java
+++ ClientSide.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package anilkali;
+
 import java.io.*;
 import java.net.*;
-/**
- *
- * @author anilk
- */
+
 public class ClientSide 
 {
     public static void main(String []args){
